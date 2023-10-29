adc2cbeb26242e93babb48111d83bcd7f13f1339
elzhankk <elzhan-96@mail.ru>
2023-10-29 22:03:17 +0600

    Initial Commit

---
+++ Devil's Bones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Devil's Bones
+//
+//  Created by elzhankk on 29.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
